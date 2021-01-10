9b51313d1ffa2821b6be7745961c883c9e5d0a1c
3156656936@qq.com <3156656936@qq.com>
2021-01-10 22:02:29 +0800

    55555

--- Purchase_Sell_Stock.IServices/Class1.cs
+++ Purchase_Sell_Stock.IServices/Class1.cs
@@ -5,7 +5,7 @@ namespace Purchase_Sell_Stock.IServices
     public class Class1
     {
         public int Name { get; set; }
-        public int AAAA { get; set; }
+        public int UUUUU { get; set; }
         public int Age { get; set; }
     }
 }
