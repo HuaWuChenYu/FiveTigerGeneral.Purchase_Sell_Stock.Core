5b999abdda8598175cb213bd1fd52162435ae88d
lenovo <lenovo@QIAO>
2021-01-11 16:16:12 +0800

    11

--- Purchase_Sell_Stock.DAL/StorageDal.cs
+++ Purchase_Sell_Stock.DAL/StorageDal.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Purchase_Sell_Stock.Model;
+using Purchase_Sell_Stock.Model.Storage;
 
 namespace Purchase_Sell_Stock.DAL
 {
@@ -9,5 +11,15 @@ namespace Purchase_Sell_Stock.DAL
     /// </summary>
     public class StorageDal
     {
+        //出库订单显示
+        public List<Outboundorder> OutboundorderShow()
+        {
+            return new List<Outboundorder>();
+        }
+        //入库订单显示
+        public List<Incomingorder> IncomingorderShow()
+        {
+            return new List<Incomingorder>();
+        }
     }
 }
