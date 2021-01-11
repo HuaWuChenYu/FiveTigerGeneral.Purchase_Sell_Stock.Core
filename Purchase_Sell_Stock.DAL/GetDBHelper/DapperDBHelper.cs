e3d7209c3ca1a0e6a4e450e83894eeffd2e742dc
lenovo <lenovo@QIAO>
2021-01-11 16:31:32 +0800

    1

--- Purchase_Sell_Stock.DAL/GetDBHelper/DapperDBHelper.cs
+++ Purchase_Sell_Stock.DAL/GetDBHelper/DapperDBHelper.cs
@@ -6,20 +6,6 @@ namespace Purchase_Sell_Stock.DAL.GetDBHelper
 {
     public class DapperDBHelper : DBHelper
     {
-        string connStr = "";
-        public override int ExecuteNonQuery()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override object ExecuteScalar()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override List<T> GetList<T>()
-        {
-            return new List<T>();
-        }
+        
     }
 }
