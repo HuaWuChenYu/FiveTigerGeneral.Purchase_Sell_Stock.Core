8c63bb72aaae00fdcf04324820887ac9a7fc6f43
ZX <1127104464@qq.com>
2021-01-11 12:01:31 +0800

    配置api跨域

--- Purchase_Sell_Stock.API/Startup.cs
+++ Purchase_Sell_Stock.API/Startup.cs
@@ -24,6 +24,16 @@ namespace Purchase_Sell_Stock.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //配置跨域处理，允许所有来源：
+            services.AddCors(options =>
+            {
+                options.AddPolicy("any", builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod()   //必须写AllowAnyHeader否则前端掉不了 
+                    //.AllowCredentials()//指定处理cookie
+                .AllowAnyOrigin(); //允许任何来源的主机访问
+                });
+            });
             services.AddControllers();
         }
 
@@ -34,6 +44,7 @@ namespace Purchase_Sell_Stock.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors("any");
 
             app.UseRouting();
 
