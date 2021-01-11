d7d2a681d0a5ed1453731310fc9ad431fdd3a7bd
ZX <1127104464@qq.com>
2021-01-11 15:19:50 +0800

    zx

--- Purchase_Sell_Stock.Model/OrderFunction/CancelOrder.cs
+++ Purchase_Sell_Stock.Model/OrderFunction/CancelOrder.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace Purchase_Sell_Stock.Model.OrderFunction
 {
+    /// <summary>
+    /// 退单表
+    /// </summary>
     public class CancelOrder
     {
+        /// <summary>
+        /// 退单主键
+        /// </summary>
+        public int CancelOrderId { get; set; }
+        /// <summary>
+        /// 退单时间
+        /// </summary>
+        public DateTime CancelOrderTime { get; set; }
+        /// <summary>
+        /// 退单申请时间
+        /// </summary>
+        public DateTime CancelOrderApplyTime { get; set; }
+        /// <summary>
+        /// 退款状态
+        /// </summary>
+        public int CancelOrderState { get; set; }
+        /// <summary>
+        /// 退款原因
+        /// </summary>
+        public string CancelOrderReason { get; set; }
+        /// <summary>
+        /// 退款图片
+        /// </summary>
+        public string CancelOrderPhoto { get; set; }
+        /// <summary>
+        /// 订单外键
+        /// </summary>
+        public int OrdersId { get; set; }
     }
 }
--- Purchase_Sell_Stock.Model/OrderFunction/Reply.cs
+++ Purchase_Sell_Stock.Model/OrderFunction/Reply.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Purchase_Sell_Stock.Model.OrderFunction
 {
+    /// <summary>
+    /// 回复评论表
+    /// </summary>
     public class Reply
     {
+        /// <summary>
+        /// 回复评论主键
+        /// </summary>
+        public int ReplyId { get; set; }
+        /// <summary>
+        /// 回复内容
+        /// </summary>
+        public string ReplyContent { get; set; }
+        /// <summary>
+        /// 评论外键
+        /// </summary>
+        public int CommentId { get; set; }
     }
 }
