1e567ac24c647b62ce4b91ea3fa5ff7df1546cd9
武晨宇 <2012837663@qq.com>
2021-01-11 10:50:05 +0800

    设置表更改

---
+++ Purchase_Sell_Stock.Model/SettingModels/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Purchase_Sell_Stock.Model.SettingModels
+{
+    /// <summary>
+    /// 部门表
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// 部门主键
+        /// </summary>
+        public int DepartmentId { get; set; }
+        /// <summary>
+        /// 部门编号
+        /// </summary>
+        public string DepartmentNumber { get; set; }
+        /// <summary>
+        /// 部门名称
+        /// </summary>
+        public string DepartmentName { get; set; }
+        /// <summary>
+        /// 部门父id
+        /// </summary>
+        public int DepartmentParentId { get; set; }
+    }
+}
---
+++ Purchase_Sell_Stock.Model/SettingModels/EmployeeAndStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Purchase_Sell_Stock.Model.SettingModels
+{
+    public class EmployeeAndStore
+    {
+        
+    }
+}
