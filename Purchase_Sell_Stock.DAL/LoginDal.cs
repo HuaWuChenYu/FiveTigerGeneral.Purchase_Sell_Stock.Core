3373e54dfbb4c2b4d7b1688c6a58c66d9ee40c1b
99328 <99328@LAPTOP-180LG0E3>
2021-01-11 11:59:39 +0800

    dh1

--- Purchase_Sell_Stock.DAL/LoginDal.cs
+++ Purchase_Sell_Stock.DAL/LoginDal.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Purchase_Sell_Stock.Model.SettingModels;
 
 namespace Purchase_Sell_Stock.DAL
 {
     public class LoginDal
     {
+        GetDBHelper.DBHelper db = new GetDBHelper.DBHelper();
+        public List<Users> Login(string name, string pwd)
+        {
+            string sql = "";
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(pwd))
+            {
+                sql = $"select * from yh_Users where UserAccount='{name}' and UserPassword='{pwd}'";
+            }
+            return db.GetList<Users>(sql);
+        }
     }
 }
