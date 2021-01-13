24f192b10cb2368847c099d632149b98fd5f5ea1
99328 <99328@LAPTOP-180LG0E3>
2021-01-13 16:58:07 +0800

    dh6

--- Purchase_Sell_Stock.IServices/ICustomer.cs
+++ Purchase_Sell_Stock.IServices/ICustomer.cs
@@ -11,7 +11,15 @@ namespace Purchase_Sell_Stock.IServices
         /// 全部用户查询
         /// </summary>
         List<T> GetCustomers<T>(int customerId, string customerName, string customerPhone, string customerIdentity, int lableId, string whetherEnable);
-
+        /// <summary>
+        /// 充值记录查询
+        /// </summary>
+        /// <param name="customerName"></param>
+        /// <param name="customerPhone"></param>
+        /// <param name="denominationId"></param>
+        /// <returns></returns>
         List<RechargeRecord> GetRechargeRecord(string customerName, string customerPhone, int denominationId);
+
+        //List<>
     }
 }
