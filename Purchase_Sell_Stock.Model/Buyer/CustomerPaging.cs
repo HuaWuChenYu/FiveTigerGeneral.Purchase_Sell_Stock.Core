be345c06b9fc76250e8393f061578b5efbffdd11
99328 <99328@LAPTOP-180LG0E3>
2021-01-13 11:28:55 +0800

    4

---
+++ Purchase_Sell_Stock.Model/Buyer/CustomerPaging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Purchase_Sell_Stock.Model.Buyer;
+
+namespace Purchase_Sell_Stock.Model.Buyer
+{
+    /// <summary>
+    /// 买家表分页
+    /// </summary>
+    public class CustomerPaging
+    {
+        public List<Customer> list { get; set; }
+        public int Count { get; set; }
+    }
+}
--- Purchase_Sell_Stock.Services/LoginBll.cs
+++ Purchase_Sell_Stock.Services/LoginBll.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Purchase_Sell_Stock.Model.SettingModels;
+using Purchase_Sell_Stock.DAL;
 
 namespace Purchase_Sell_Stock.Services
 {
     public class LoginBll
     {
+        LoginDal dal = new LoginDal();
+
+        public List<Users> Login(string name, string pwd)
+        {
+            return dal.Login(name, pwd);
+        }
     }
 }
